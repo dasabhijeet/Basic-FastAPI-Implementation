7a9e5999d3148f934f47bc8a2c29780e0c334fdc
Abhijeet D <143497155+dasabhijeet@users.noreply.github.com>
2025-11-12 18:02:08 +0530

    Add files via upload

---
+++ app/main.py
@@ -0,0 +1,11 @@
+# import libraries
+from fastapi import FastAPI
+from api.routes_user import router as user_router
+import uvicorn
+
+app = FastAPI()
+
+app.include_router(user_router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
